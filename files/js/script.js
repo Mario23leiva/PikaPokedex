e6ce6bfa9a50391db4d722656b47fedf02784d4b
Mario23leiva <mleiva@alumno.santoangel.org>
2023-06-07 20:28:50 +0200

    dsf

--- files/js/script.js
+++ files/js/script.js
@@ -8,6 +8,10 @@ const input = document.querySelector('.input__search');
 
 const buttonPrev = document.querySelector('.btn__left');
 const buttonNext = document.querySelector('.btn__right');
+const buttonUp = document.querySelector('.btn__up');
+const buttonDown = document.querySelector('.btn__down');
+const buttonA = document.querySelector('.btn__a');
+const buttonB = document.querySelector('.btn__b');
 
 var pokemonSelected = '';
 var index = 1;
@@ -25,30 +29,35 @@ const fetchLista = async () => {
 }
 
 const renderList = async (list) => {
-    list.results.forEach(async pokemon => {
+    for (let i = 0; i < list.results.length; i++) {
+        const pokemon = list.results[i];
         const poke = await fetchPokemon(pokemon.name);
-        const pokemonSprite = poke['sprites']['versions']['generation-viii']['icons']['front_default'];
+        const pokemonSprite = poke.sprites.versions['generation-viii'].icons.front_default;
         const listItem = document.createElement('li');
-        if(index == indexSelected){
+    
+        if (index === indexSelected) {
             listItem.className = 'active';
         } else {
             listItem.className = '#' + index;
         }
+        
         listItem.id = index;
         listItem.innerHTML = `
-          <img src="${pokemonSprite}"/>
-          <span>${pokemon.name}</span>
-        `;
-        pokeAPIList.appendChild(listItem);
-        index++;
-    });
-
+            <img src="${pokemonSprite}"/>
+            <span>${pokemon.name}</span>
+            `;
+        
+            pokeAPIList.appendChild(listItem);
+            index++;
+    }
+  
     const element001 = document.getElementById("1");
     if (element001) {
         element001.className = "active";
     }
+};
+  
 
-}
 
 
 const fetchPokemon = async (pokemon) => {
@@ -104,6 +113,41 @@ buttonNext.addEventListener('click' , () =>{
 buttonPrev.addEventListener('click' , () =>{
     var numberPokemonSelected = parseInt(pokemonSelected);
     renderPokemon((numberPokemonSelected - 1));
+})
+
+buttonUp.addEventListener('click' , () =>{
+    if(indexSelected != 1){
+        const elementSelected = document.querySelector(".active");
+        elementSelected.classList.replace("active", "#" + indexSelected);
+        indexSelected--;
+        const newElementSelected = document.getElementById(indexSelected);
+        newElementSelected.classList.replace("#" + indexSelected, "active");
+        newElementSelected.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    } else {
+        //poner ultimo pokemon seleccionado
+    }
+})
+
+buttonDown.addEventListener('click' , () =>{
+    if(indexSelected != 898){
+        const elementSelected = document.querySelector(".active");
+        elementSelected.classList.replace("active", "#" + indexSelected);
+        indexSelected++;
+        const newElementSelected = document.getElementById(indexSelected);
+        newElementSelected.classList.replace("#" + indexSelected, "active");
+        newElementSelected.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    } else {
+        //poner primer pokemon seleccionado
+    }
+})
+
+buttonA.addEventListener('click' , () =>{
+    const elementSelected = document.querySelector(".active");
+    let idSelected = null;
+    if (elementSelected && elementSelected.id) {
+        idSelected = elementSelected.id;
+        renderPokemon(elementSelected.id);
+    }
 })
 
 renderPokemon('1');
